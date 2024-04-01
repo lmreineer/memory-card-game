e4f87551d275c0150978baec2708375276b52761
lance <lmreineer@gmail.com>
2024-04-01 10:45:30 +0800

    Modify useState hook's  initial function

--- src/App.js
+++ src/App.js
@@ -2,32 +2,38 @@ import React, { useState } from "react";
 import ImageCard from "./ImageCard";
 
 export default function App() {
-  const [imageList, setImageList] = useState(() => {
-    // Import images from the assets folder
-    const images = require.context("./assets", true);
-    return images.keys().map((image) => images(image));
-  });
+  // Initially shuffle the images
+  const [imageList, setImageList] = useState(shuffleImages);
 
   // Function for shuffling the images
   function shuffleImages() {
-    setImageList((prevImageList) => {
-      const shuffledList = [...prevImageList];
-      for (let i = shuffledList.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
-      }
-      return shuffledList;
-    });
+    const images = require.context("./assets", true);
+    const shuffledList = images.keys().map((image) => images(image));
+
+    // Algorithm for shuffling
+    for (let i = shuffledList.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
+    }
+
+    return shuffledList;
   }
 
-  const imageCards = imageList.map((src, index) => (
-    <ImageCard
-      key={index}
-      index={index + 1}
-      src={src}
-      onClick={shuffleImages}
-    />
-  ));
+  function handleImageClick() {
+    // Shuffle the images when clicked
+    setImageList(shuffleImages);
+  }
+
+  const imageCards = imageList
+    .slice(0, 3)
+    .map((src, index) => (
+      <ImageCard
+        key={index}
+        index={index + 1}
+        src={src}
+        onClick={handleImageClick}
+      />
+    ));
 
   return (
     <div className="flex h-screen flex-col justify-around">
